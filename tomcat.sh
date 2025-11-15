f6bd2e727a0d24ff251972b78a62c1d9e7ec5edc
sravanbommareddy <sravanbommareddy05@gmail.com>
2025-11-15 14:29:43 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -3,7 +3,7 @@ wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.112/bin/apache-tomcat-9.0.112
 tar -zxvf apache-tomcat-9.0.112.tar.gz
 sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.112/conf/tomcat-users.xml
 sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.112/conf/tomcat-users.xml
-sed -i '58  a\<user username="tomcat" password="admin@123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.112/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="admin@1234" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.112/conf/tomcat-users.xml
 sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.112/conf/tomcat-users.xml
 sed -i '56d' apache-tomcat-9.0.112/conf/tomcat-users.xml
 sed -i '21d' apache-tomcat-9.0.112/webapps/manager/META-INF/context.xml
